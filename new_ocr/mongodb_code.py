25d6f135f52a1a05f7c5b579b41ea24c0267ec3b
Shubham Bhoilkar <91750972+shubhambhoilkar@users.noreply.github.com>
2025-12-03 17:21:54 +0530

    Update mongodb_code.py

--- new_ocr/mongodb_code.py
+++ new_ocr/mongodb_code.py
@@ -1,41 +1,45 @@
 import pandas as pd
 from pymongo import MongoClient
 
-# 1. Load Excel
-file_path = "output.xlsx"
-df= pd.read_excel(file_path)
+try:
+    # 1. Load Excel
+    file_path = "C:\\Users\\Developer\\Shubham_files\\ocr\\output.xlsx"
+    df= pd.read_excel(file_path)
+    print(df.to_string())
+    #Additional FIX
+    df.columns = df.columns.map(str)
 
-#Additional FIX
-df.columns = df.columns.map(str)
+    # 2. Identify column groups
+    payslips_cols = [c for c in df.columns if c.startswith("PAYSLIP_")]
+    timesheet_cols = [c for c in df.columns if c.startswith("TIMESHEET_")]
 
-# 2. Identify column groups
-payslips_cols = [c for c in df.columns if c.startswith("PAYSLIP_")]
-timesheet_cols = [c for c in df.columns if c.startswith("TIMESHEET_")]
+    identify_cols = ["EMPLOYEE_NAME", "PASSPORT_NUMBER"]
+    identify_cols = [c for c in identify_cols if c in df.columns]
 
-identify_cols = ["EMPLOYEE_NAME", "PASSPORT_NUMBER"]
-identify_cols = [c for c in identify_cols if c in df.columns]
+    # 3. Slip into the names for MongoDB
+    df_payslip = df[identify_cols + payslips_cols].copy()
+    df_timesheet = df[identify_cols + timesheet_cols].copy()
 
-# 3. Slip into the names for MongoDB
-df_payslip = df[identify_cols + payslips_cols].copy()
-df_timesheet = df[identify_cols + timesheet_cols].copy()
+    # 4. Clean column names for MongoDB
+    df_payslip.columns = [c.lower() for c in df_payslip.columns]
+    df_timesheet.columns = [c.lower() for c in df_timesheet.columns]
 
-# 4. Clean column names for MongoDB
-df_payslip.columns = [c.lower() for c in df_payslip.columns]
-df_timesheet.columns = [c.lower() for c in df_timesheet.columns]
+    df_payslip = df_payslip.where(pd.notnull(df_payslip), None)
+    df_timesheet = df_timesheet.where(pd.notnull(df_timesheet), None)
 
-df_payslip = df_payslip.where(pd.notnull(df_payslip), None)
-df_timesheet = df_timesheet.where(pd.notnull(df_timesheet), None)
+    # 5. MongoDB Nan -> None for MongoDB
+    client = MongoClient("mongodb://localhost:27017")
+    db = client["OCR_Database"]
 
-# 5. MongoDB Nan -> None for MongoDB
-client = MongoClient("mongodb://localhost:27017")
-db = client["OCR_Database"]
+    payslip_collection = db["Payslip_Data"]
+    timesheet_collection = db["Timesheet_Data"]
 
-payslip_collection = db["Payslip_Data"]
-timesheet_collection = db["Timesheet_Data"]
+    # 6 . Insert into MongoDB:
+    payslip_data = df_payslip.to_dict(orient="records")
+    timesheet_data = df_timesheet.to_dict(orient="records")
 
-# 6 . Insert into MongoDB:
-payslip_data = df_payslip.to_dict(orient="records")
-timesheet_data = df_timesheet.to_dict(orient="records")
+except Exception as e:
+    print("Error at getting the sheet data. ", e)
 
 try:
     if payslip_data:
